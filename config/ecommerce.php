7e286ee5e1f6236c40a1248b8b0e46b5a637508b
AMMAR NAASS | عمار <amarnaas1981@gmail.com>
2026-08-19 09:49:28 +0100

    feat(shipping): Update Algerian states and shipping zones with all 69 official wilayas

--- config/ecommerce.php
+++ config/ecommerce.php
@@ -33,7 +33,7 @@ return [
                 '01' => 'أدرار', '02' => 'الشلف', '03' => 'الأغواط', '04' => 'أم البواقي',
                 '05' => 'باتنة', '06' => 'بجاية', '07' => 'بسكرة', '08' => 'بشار',
                 '09' => 'البليدة', '10' => 'البويرة', '11' => 'تمنراست', '12' => 'تبسة',
-                '13' => 'تلمسان', '14' => 'تيارت', '15' => 'تيزي وزو', '16' => 'الجزائر',
+                '13' => 'تلمسان', '14' => 'تيارت', '15' => 'تيزي وزو', '16' => 'الجزائر العاصمة',
                 '17' => 'الجلفة', '18' => 'جيجل', '19' => 'سطيف', '20' => 'سعيدة',
                 '21' => 'سكيكدة', '22' => 'سيدي بلعباس', '23' => 'عنابة', '24' => 'قالمة',
                 '25' => 'قسنطينة', '26' => 'المدية', '27' => 'مستغانم', '28' => 'المسيلة',
@@ -42,6 +42,12 @@ return [
                 '37' => 'تندوف', '38' => 'تيسمسيلت', '39' => 'الوادي', '40' => 'خنشلة',
                 '41' => 'سوق أهراس', '42' => 'تيبازة', '43' => 'ميلة', '44' => 'عين الدفلى',
                 '45' => 'النعامة', '46' => 'عين تموشنت', '47' => 'غرداية', '48' => 'غليزان',
+                '49' => 'تيميمون', '50' => 'برج باجي مختار', '51' => 'أولاد جلال', '52' => 'بني عباس',
+                '53' => 'عين صالح', '54' => 'عين قزام', '55' => 'تقرت', '56' => 'جانت',
+                '57' => 'المغير', '58' => 'المنيعة', '59' => 'آفلو', '60' => 'بريكة',
+                '61' => 'القنطرة', '62' => 'بئر العاتر', '63' => 'العريشة', '64' => 'قصر الشلالة',
+                '65' => 'عين وسارة', '66' => 'مسعد', '67' => 'قصر البخاري', '68' => 'بوسعادة',
+                '69' => 'الأبيض سيدي الشيخ',
             ],
         ],
     ],
@@ -84,7 +90,7 @@ return [
 
         'zones' => [
             // ===== المدن الكبرى (400 د.ج) =====
-            ['name' => 'الجزائر', 'countries' => ['DZ'], 'cities' => ['16'], 'cost' => 400, 'express_cost' => 700, 'free_threshold' => 5000],
+            ['name' => 'الجزائر العاصمة', 'countries' => ['DZ'], 'cities' => ['16'], 'cost' => 400, 'express_cost' => 700, 'free_threshold' => 5000],
             ['name' => 'وهران', 'countries' => ['DZ'], 'cities' => ['31'], 'cost' => 400, 'express_cost' => 700, 'free_threshold' => 5000],
             ['name' => 'قسنطينة', 'countries' => ['DZ'], 'cities' => ['25'], 'cost' => 400, 'express_cost' => 700, 'free_threshold' => 5000],
             ['name' => 'عنابة', 'countries' => ['DZ'], 'cities' => ['23'], 'cost' => 400, 'express_cost' => 700, 'free_threshold' => 5000],
@@ -118,8 +124,13 @@ return [
             ['name' => 'خنشلة', 'countries' => ['DZ'], 'cities' => ['40'], 'cost' => 500, 'express_cost' => 800, 'free_threshold' => 5000],
             ['name' => 'تيسمسيلت', 'countries' => ['DZ'], 'cities' => ['38'], 'cost' => 500, 'express_cost' => 800, 'free_threshold' => 5000],
             ['name' => 'معسكر', 'countries' => ['DZ'], 'cities' => ['29'], 'cost' => 500, 'express_cost' => 800, 'free_threshold' => 5000],
+            ['name' => 'قصر البخاري', 'countries' => ['DZ'], 'cities' => ['67'], 'cost' => 500, 'express_cost' => 800, 'free_threshold' => 5000],
+            ['name' => 'العريشة', 'countries' => ['DZ'], 'cities' => ['63'], 'cost' => 500, 'express_cost' => 800, 'free_threshold' => 5000],
+            ['name' => 'قصر الشلالة', 'countries' => ['DZ'], 'cities' => ['64'], 'cost' => 500, 'express_cost' => 800, 'free_threshold' => 5000],
+            ['name' => 'عين وسارة', 'countries' => ['DZ'], 'cities' => ['65'], 'cost' => 500, 'express_cost' => 800, 'free_threshold' => 5000],
+            ['name' => 'بوسعادة', 'countries' => ['DZ'], 'cities' => ['68'], 'cost' => 500, 'express_cost' => 800, 'free_threshold' => 5000],
 
-            // ===== الولايات الداخلية والصعبة الوصول (600 د.ج) =====
+            // ===== الولايات الداخلية والهضاب (600 د.ج) =====
             ['name' => 'باتنة', 'countries' => ['DZ'], 'cities' => ['05'], 'cost' => 600, 'express_cost' => 950, 'free_threshold' => 5000],
             ['name' => 'الجلفة', 'countries' => ['DZ'], 'cities' => ['17'], 'cost' => 600, 'express_cost' => 950, 'free_threshold' => 5000],
             ['name' => 'أم البواقي', 'countries' => ['DZ'], 'cities' => ['04'], 'cost' => 600, 'express_cost' => 950, 'free_threshold' => 5000],
@@ -131,13 +142,29 @@ return [
             ['name' => 'البيض', 'countries' => ['DZ'], 'cities' => ['32'], 'cost' => 600, 'express_cost' => 950, 'free_threshold' => 5000],
             ['name' => 'سعيدة', 'countries' => ['DZ'], 'cities' => ['20'], 'cost' => 600, 'express_cost' => 950, 'free_threshold' => 5000],
             ['name' => 'ورقلة', 'countries' => ['DZ'], 'cities' => ['30'], 'cost' => 600, 'express_cost' => 950, 'free_threshold' => 5000],
+            ['name' => 'أولاد جلال', 'countries' => ['DZ'], 'cities' => ['51'], 'cost' => 600, 'express_cost' => 950, 'free_threshold' => 5000],
+            ['name' => 'المغير', 'countries' => ['DZ'], 'cities' => ['57'], 'cost' => 600, 'express_cost' => 950, 'free_threshold' => 5000],
+            ['name' => 'تقرت', 'countries' => ['DZ'], 'cities' => ['55'], 'cost' => 600, 'express_cost' => 950, 'free_threshold' => 5000],
+            ['name' => 'بريكة', 'countries' => ['DZ'], 'cities' => ['60'], 'cost' => 600, 'express_cost' => 950, 'free_threshold' => 5000],
+            ['name' => 'القنطرة', 'countries' => ['DZ'], 'cities' => ['61'], 'cost' => 600, 'express_cost' => 950, 'free_threshold' => 5000],
+            ['name' => 'بئر العاتر', 'countries' => ['DZ'], 'cities' => ['62'], 'cost' => 600, 'express_cost' => 950, 'free_threshold' => 5000],
+            ['name' => 'مسعد', 'countries' => ['DZ'], 'cities' => ['66'], 'cost' => 600, 'express_cost' => 950, 'free_threshold' => 5000],
+            ['name' => 'آفلو', 'countries' => ['DZ'], 'cities' => ['59'], 'cost' => 600, 'express_cost' => 950, 'free_threshold' => 5000],
+            ['name' => 'الأبيض سيدي الشيخ', 'countries' => ['DZ'], 'cities' => ['69'], 'cost' => 600, 'express_cost' => 950, 'free_threshold' => 5000],
 
-            // ===== الولايات الصحراوية (700 د.ج) =====
+            // ===== الولايات الصحراوية والجنوبية (700 - 900 د.ج) =====
             ['name' => 'أدرار', 'countries' => ['DZ'], 'cities' => ['01'], 'cost' => 700, 'express_cost' => 1100, 'free_threshold' => 5000],
-            ['name' => 'تمنراست', 'countries' => ['DZ'], 'cities' => ['11'], 'cost' => 700, 'express_cost' => 1100, 'free_threshold' => 5000],
-            ['name' => 'تندوف', 'countries' => ['DZ'], 'cities' => ['37'], 'cost' => 700, 'express_cost' => 1100, 'free_threshold' => 5000],
+            ['name' => 'تمنراست', 'countries' => ['DZ'], 'cities' => ['11'], 'cost' => 800, 'express_cost' => 1200, 'free_threshold' => 5000],
+            ['name' => 'تندوف', 'countries' => ['DZ'], 'cities' => ['800'], 'cost' => 800, 'express_cost' => 1200, 'free_threshold' => 5000],
             ['name' => 'غرداية', 'countries' => ['DZ'], 'cities' => ['47'], 'cost' => 700, 'express_cost' => 1100, 'free_threshold' => 5000],
-            ['name' => 'إليزي', 'countries' => ['DZ'], 'cities' => ['33'], 'cost' => 700, 'express_cost' => 1100, 'free_threshold' => 5000],
+            ['name' => 'إليزي', 'countries' => ['DZ'], 'cities' => ['33'], 'cost' => 800, 'express_cost' => 1200, 'free_threshold' => 5000],
+            ['name' => 'تيميمون', 'countries' => ['DZ'], 'cities' => ['49'], 'cost' => 700, 'express_cost' => 1100, 'free_threshold' => 5000],
+            ['name' => 'برج باجي مختار', 'countries' => ['DZ'], 'cities' => ['50'], 'cost' => 900, 'express_cost' => 1300, 'free_threshold' => 5000],
+            ['name' => 'بني عباس', 'countries' => ['DZ'], 'cities' => ['52'], 'cost' => 700, 'express_cost' => 1100, 'free_threshold' => 5000],
+            ['name' => 'عين صالح', 'countries' => ['DZ'], 'cities' => ['53'], 'cost' => 800, 'express_cost' => 1200, 'free_threshold' => 5000],
+            ['name' => 'عين قزام', 'countries' => ['DZ'], 'cities' => ['54'], 'cost' => 900, 'express_cost' => 1300, 'free_threshold' => 5000],
+            ['name' => 'جانت', 'countries' => ['DZ'], 'cities' => ['56'], 'cost' => 900, 'express_cost' => 1300, 'free_threshold' => 5000],
+            ['name' => 'المنيعة', 'countries' => ['DZ'], 'cities' => ['58'], 'cost' => 700, 'express_cost' => 1100, 'free_threshold' => 5000],
         ],
 
         'companies' => [
